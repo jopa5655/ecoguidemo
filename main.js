b448a94c9e11ce5f77e3ec81be24841e0772a39c
jopa5655 <75279427+jopa5655@users.noreply.github.com>
2022-04-23 13:56:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -176,11 +176,9 @@ document.getElementById("organizacije").style.background = "transparent";
 }
 }
 
-var myRequest = new Request('HTTPS://api.airvisual.com/v2/city?city=Mostar&state=federation-of-b-h&country=bosnia-herzegovina&key=9aad6d8a-c216-4aef-9282-b7c0cdc6a347');
-var myMode = myRequest.mode; // returns "cors" by default
-
-let api = "HTTPS://api.airvisual.com/v2/city?city=Mostar&state=federation-of-b-h&country=bosnia-herzegovina&key=9aad6d8a-c216-4aef-9282-b7c0cdc6a347";
-	fetch(api)
+	fetch('HTTPS://api.airvisual.com/v2/city?city=Mostar&state=federation-of-b-h&country=bosnia-herzegovina&key=9aad6d8a-c216-4aef-9282-b7c0cdc6a347', { 
+    mode: 'no-cors' // 'cors' by default
+})
 	.then(function(response){
 		const data = response.json();
 		console.log(data);
